4e4c8e50f777f8081edd3e00804a0a8e6297731d
SimonBarta <124711984+SimonBarta@users.noreply.github.com>
2023-02-28 10:58:12 +0100

    Update tb_mux_3bit_4to1.vhd

--- 03-vivado/multiplexer/multiplexer.srcs/sim_1/new/tb_mux_3bit_4to1.vhd
+++ 03-vivado/multiplexer/multiplexer.srcs/sim_1/new/tb_mux_3bit_4to1.vhd
@@ -1,83 +1,104 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 02/22/2023 12:36:42 PM
--- Design Name: 
--- Module Name: tb_mux_3bit_4to1 - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
+library ieee;
+use ieee.std_logic_1164.all;
+------------------------------------------------------------
+-- Entity declaration for testbench
+------------------------------------------------------------
 entity tb_mux_3bit_4to1 is
---  Port ( );
-end tb_mux_3bit_4to1;
+    -- Entity of testbench is always empty
+end entity tb_mux_3bit_4to1;
 
-architecture Behavioral of tb_mux_3bit_4to1 is
-p_label : process (a_i)
-begin
-    case a_i is
-        when '0' =>
-            q <= '0';
-        when others =>
-            q <= '1';
-    end case;
-end process p_label;
-begin
+------------------------------------------------------------
+-- Architecture body for testbench
+------------------------------------------------------------
+architecture testbench of tb_mux_3bit_4to1 is
 
-p_label : process (b_i)
-begin
-    case b_i is
-        when '0' =>
-            q <= '0';
-        when others =>
-            q <= '1';
-    end case;
-end process p_label;
-begin
+    -- Local signals
+    signal s_a_i           : std_logic_vector(2 downto 0);
+    signal s_b_i           : std_logic_vector(2 downto 0);
+    signal s_c_i          : std_logic_vector(2 downto 0);
+    signal s_d_i           : std_logic_vector(2 downto 0);
+    signal s_f_i           : std_logic_vector(2 downto 0);
+    signal s_sel_i         : std_logic_vector(1 downto 0);
 
-p_label : process (c_i)
 begin
-    case c_i is
-        when '0' =>
-            q <= '0';
-        when others =>
-            q <= '1';
-    end case;
-end process p_label;
-begin
+  
+    uut_mux_3bit_4to1 : entity work.mux_3bit_4to1
+        port map(
+            a_i           => s_a_i,
+            b_i           => s_b_i,
+            c_i => s_c_i,
+            d_i => s_d_i,
+            sel_i => s_sel_i,
+            f_i => s_f_i
+        );
 
-p_label : process (d_i)
-begin
-    case d_i is
-        when '0' =>
-            q <= '0';
-        when others =>
-            q <= '1';
-    end case;
-end process p_label;
-begin
+    --------------------------------------------------------
+    -- Data generation process
+    --------------------------------------------------------
+        p_stimulus : process
+    begin
+        -- Report a note at the beginning of stimulus process
+        report "Stimulus process started" severity note;
+        
+        --First test case
+        s_a_i <= "000"; 
+        s_b_i <= "001";
+        s_c_i <= "010";  
+        s_d_i <= "011";  
+        s_sel_i <= "00";  
+                  
+        wait for 100 ns;
+       
+       -- Expected output
+        assert s_f_i = "000"
+        -- Error report
+        report "Input combination s_sel_i = 00 FAILED" severity error;
 
-end Behavioral;
+        --Second test case
+        s_a_i <= "000"; 
+        s_b_i <= "001";
+        s_c_i <= "010";  
+        s_d_i <= "011";  
+        s_sel_i <= "01";  
+                  
+        wait for 100 ns;
+       
+       -- Expected output
+        assert s_f_i = "001"
+        -- Error report
+        report "Input combination s_sel_i = 01 FAILED" severity error;
+        
+        --Third test case
+        s_a_i <= "000"; 
+        s_b_i <= "001";
+        s_c_i <= "010";  
+        s_d_i <= "011";  
+        s_sel_i <= "10";  
+                  
+        wait for 100 ns;
+       
+       -- Expected output
+        assert s_f_i = "010"
+        -- Error report
+        report "Input combination s_sel_i 10 FAILED" severity error;
+	
+        --Fourth test case
+        s_a_i <= "000"; 
+        s_b_i <= "001";
+        s_c_i <= "010";  
+        s_d_i <= "011";  
+        s_sel_i <= "11";  
+                  
+        wait for 100 ns;
+        
+        -- Expected output
+        assert s_f_i = "011"
+        -- Error report
+        report "Input combination s_sel_i = 11 FAILED" severity error;
+	
+	-- Report of finising process
+        report "Process finished" severity note;
+        wait;
+    end process p_stimulus;
+
+end architecture testbench;
